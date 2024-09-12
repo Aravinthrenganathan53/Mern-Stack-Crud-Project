994f5acc9aeba1d4e2734eafcfcd4958b46b325f
Aravinthrenganathan53 <aravinthrenganathan55@gmail.com>
2024-09-12 08:21:17 +0530

    All Files Uploaded

---
+++ Backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const dotenv = require("dotenv")
+const mongoose = require("mongoose");
+const app = express();
+const taskRoutes = require("./routes/taskRoute");
+const cors = require('cors')
+
+dotenv.config();
+
+//Middleware
+app.use((req, res, next) => {
+  console.log("path " + req.path + " method " + req.method);
+  next();
+});
+
+app.use(cors())
+
+app.use(express.json());
+// app.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
+
+// DB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        "DB connected Successfully and listening to " + process.env.PORT
+      );
+    });
+  })
+  .catch((error) => console.log(error));
+
+app.use("/api/tasks", taskRoutes);
