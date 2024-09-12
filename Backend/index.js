1fb3ef781f4193fa3d75bc3334022e3f3f715359
Aravinthrenganathan53 <aravinthrenganathan55@gmail.com>
2024-09-12 08:26:39 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,20 +6,7 @@ const taskRoutes = require("./routes/taskRoute");
 const cors = require('cors')
 
 dotenv.config();
-
-//Middleware
-app.use((req, res, next) => {
-  console.log("path " + req.path + " method " + req.method);
-  next();
-});
-
 app.use(cors())
-
-app.use(express.json());
-// app.get("/", (req, res) => {
-//   res.send("Hello world");
-// });
-
 // DB connection
 mongoose
   .connect(process.env.MONGO_URI)
